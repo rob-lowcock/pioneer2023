d4e9458c571f73e579c62300a6f56e36150e5a5e
Rob Lowcock <rob.lowcock@gmail.com>
2023-05-25 17:19:06 +0100

    Add emoji picker

--- client/src/retro/retroaddbutton.tsx
+++ client/src/retro/retroaddbutton.tsx
@@ -1,23 +1,48 @@
+import { FaceSmileIcon } from "@heroicons/react/24/outline";
 import { PlusIcon, XMarkIcon } from "@heroicons/react/24/solid";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import Picker from '@emoji-mart/react';
 
 export default function RetroAddButton() {
     const [showForm, setShowForm] = useState(false);
+    const [showPicker, setShowPicker] = useState(false);
+    const inputRef = useRef<HTMLInputElement>(null);
 
-    const onClick = (e : React.MouseEvent<HTMLAnchorElement>) => {
+    const toggleForm = (e : React.MouseEvent<HTMLAnchorElement>) => {
         setShowForm(!showForm);
+        setShowPicker(false);
+    }
+
+    const togglePicker = (e : React.MouseEvent<HTMLAnchorElement>) => {
+        setShowPicker(!showPicker);
+    }
+
+    const pickerData = async () => {
+        const response = await fetch('https://cdn.jsdelivr.net/npm/@emoji-mart/data');
+        return response.json();
+    }
+
+    const pickerEvent = (emoji: any) => {
+        inputRef.current!.value += emoji.native
     }
 
     return <>
     { showForm ?
         <form action="#" className="bg-gray-200 rounded-lg p-2 mb-2">
-            <a href="#" onClick={onClick} className="float-right text-sm mb-1 text-center text-gray-500 bg-gray-200 hover:bg-gray-100 py-1 px-2 rounded"><XMarkIcon className="h-3 w-3 inline-block" /></a>
+            <a href="#" onClick={toggleForm} className="float-right text-sm mb-1 text-center text-gray-500 bg-gray-200 hover:bg-gray-100 py-1 px-2 rounded"><XMarkIcon className="h-3 w-3 inline-block" /></a>
             <p className="p-1 text-gray-500 text-sm">What's on your mind?</p>
-            <input type="text" className="block w-full rounded-md p-4 border-brdgray focus:border-slate focus:ring-0" autoFocus />
+            <input type="text" ref={inputRef} className="block w-full rounded-md p-4 border-brdgray focus:border-slate focus:ring-0" autoFocus />
+            <a href="#" title="Toggle emoji picker" className="float-left text-gray-500 mt-2" onClick={togglePicker}><FaceSmileIcon className="h-6 w-6 inline-block" /></a>
             <button type="submit" className="block rounded-md bg-gray-500 ml-auto px-4 py-2 mt-2 text-sm text-white hover:bg-webscale-lighter">Add</button>
         </form>
         :
-        <a href="#" className="block text-sm mb-2 text-center text-gray-500 bg-gray-200 hover:bg-gray-100 p-2 rounded-lg" onClick={onClick}><PlusIcon className="h-6 w-6 inline-block" /> Add something</a>
+        <a href="#" className="block text-sm mb-2 text-center text-gray-500 bg-gray-200 hover:bg-gray-100 p-2 rounded-lg" onClick={toggleForm}><PlusIcon className="h-6 w-6 inline-block" /> Add something</a>
+    }
+    { showPicker ?
+        <div className="absolute">
+        <Picker data={pickerData} onEmojiSelect={pickerEvent} />
+        </div>
+        : null
     }
     </>
 }
